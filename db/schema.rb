531bd0a5304ec6afed160d19c75335d158dc3d58
StrategyBlack <nblack0101@gmail.com>
2020-11-28 15:08:56 +0000

    Formatted forms and changed attribute name in articles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_21_211532) do
+ActiveRecord::Schema.define(version: 2020_11_28_143426) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "text"
-    t.datetime "create_at"
+    t.datetime "created_at"
     t.datetime "updated_at"
   end
 
